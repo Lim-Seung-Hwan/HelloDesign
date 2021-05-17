2236141b515daf5ad75750fb1946f43ef320939c
DIGIWB <hellojo011@ce.mokpo.ac.kr>
2021-05-17 14:53:05 +0900

    Android WebView Application Added

---
+++ hellodesign/app/src/main/java/com/example/hellodesign/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.hellodesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private long backBtnTime = 0;
+    WebView web;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        web = findViewById(R.id.wv);
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("http://digiwb.softether.net:8085/TTIShop/index.jsp");
+
+
+
+
+    }
+    @Override
+    public void onBackPressed() {
+        long curTime = System.currentTimeMillis();
+        long gapTime = curTime - backBtnTime;
+        if (web.canGoBack()) {
+            web.goBack();
+        } else if (0 <= gapTime && 2000 >= gapTime) {
+            super.onBackPressed();
+        } else {
+            backBtnTime = curTime;
+            Toast.makeText(this, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+}
