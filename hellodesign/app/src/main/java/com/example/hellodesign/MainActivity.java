467ef52d3be3b9856a32c4cf8533da54b47a1a3d
DIGIWB <hellojo011@ce.mokpo.ac.kr>
2021-06-03 10:52:34 +0900

    안드로이드 앱 소스 갱신(파일 업로드)

--- hellodesign/app/src/main/java/com/example/hellodesign/MainActivity.java
+++ hellodesign/app/src/main/java/com/example/hellodesign/MainActivity.java
@@ -1,30 +1,51 @@
 package com.example.hellodesign;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
 import android.webkit.JsResult;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import java.io.File;
 import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
     private long backBtnTime = 0;
     WebView web;
+    public ValueCallback<Uri> filePathCallbackNormal;
+    public ValueCallback<Uri[]> filePathCallbackLollipop;
+    public final static int FILECHOOSER_NORMAL_REQ_CODE = 2001;
+    public final static int FILECHOOSER_LOLLIPOP_REQ_CODE = 2002;
+    private Uri cameraImageUri = null;
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +55,14 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main_horizontal);
         }
 
+
+        checkVerify();
+
         web = findViewById(R.id.wv);
         WebSettings webSettings = web.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        // WebView.setWebContentsDebuggingEnabled(true); // 디버그 필요시 주석을 제거할 것
         web.setWebViewClient(new MyWebViewClient());
         web.loadUrl("http://digiwb.softether.net:8085/TTIShop/index.jsp");
 
@@ -45,6 +70,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
                 return super.onJsAlert(view, url, message, result);
+            }
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+                Log.d("MainActivity", "5.0+");
+
+                // Callback 초기화 (중요!)
+                if (filePathCallbackLollipop != null) {
+                    filePathCallbackLollipop.onReceiveValue(null);
+                    filePathCallbackLollipop = null;
+                }
+                filePathCallbackLollipop = filePathCallback;
+
+                boolean isCapture = fileChooserParams.isCaptureEnabled();
+                runCamera(isCapture);
+                return true;
             }
         });
 
@@ -114,5 +156,157 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
 
+    }
+
+    //권한 획득 여부 확인
+    @TargetApi(Build.VERSION_CODES.M)
+    public void checkVerify() {
+
+        if (checkSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //카메라 또는 저장공간 권한 획득 여부 확인
+            if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE) || shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                // 카메라 및 저장공간 권한 요청
+                requestPermissions(new String[]{Manifest.permission.INTERNET, Manifest.permission.CAMERA,
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            } else {
+                //startApp();
+            }
+        }
+    }
+
+    //권한 획득 여부에 따른 결과 반환
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1)
+        {
+            if (grantResults.length > 0)
+            {
+                for (int i=0; i<grantResults.length; ++i)
+                {
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED)
+                    {
+                        // 카메라, 저장소 중 하나라도 거부한다면 앱실행 불가 메세지 띄움
+                        new AlertDialog.Builder(this).setTitle("알림").setMessage("권한을 허용해주셔야 앱을 이용할 수 있습니다.")
+                                .setPositiveButton("종료", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        finish();
+                                    }
+                                }).setNegativeButton("권한 설정", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                        .setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                                getApplicationContext().startActivity(intent);
+                            }
+                        }).setCancelable(false).show();
+
+                        return;
+                    }
+                }
+                Toast.makeText(this, "Succeed Read/Write external storage !", Toast.LENGTH_SHORT).show();
+                //startApp();
+            }
+        }
+    }
+
+    //액티비티가 종료될 때 결과를 받고 파일을 전송할 때 사용
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode)
+        {
+            case FILECHOOSER_NORMAL_REQ_CODE:
+                if (resultCode == RESULT_OK)
+                {
+                    if (filePathCallbackNormal == null) return;
+                    Uri result = (data == null || resultCode != RESULT_OK) ? null : data.getData();
+                    //  onReceiveValue 로 파일을 전송한다.
+                    filePathCallbackNormal.onReceiveValue(result);
+                    filePathCallbackNormal = null;
+                }
+                break;
+            case FILECHOOSER_LOLLIPOP_REQ_CODE:
+                if (resultCode == RESULT_OK)
+                {
+                    if (filePathCallbackLollipop == null) return;
+                    if (data == null)
+                        data = new Intent();
+                    if (data.getData() == null)
+                        data.setData(cameraImageUri);
+
+                    filePathCallbackLollipop.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+                    filePathCallbackLollipop = null;
+                }
+                else
+                {
+                    if (filePathCallbackLollipop != null)
+                    {   //  resultCode에 RESULT_OK가 들어오지 않으면 null 처리하지 한다.(이렇게 하지 않으면 다음부터 input 태그를 클릭해도 반응하지 않음)
+                        filePathCallbackLollipop.onReceiveValue(null);
+                        filePathCallbackLollipop = null;
+                    }
+
+                    if (filePathCallbackNormal != null)
+                    {
+                        filePathCallbackNormal.onReceiveValue(null);
+                        filePathCallbackNormal = null;
+                    }
+                }
+                break;
+            default:
+
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    // 카메라 기능 구현
+    private void runCamera(boolean _isCapture)
+    {
+        Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        File path = getFilesDir();
+        File file = new File(path, "sample.png"); // sample.png 는 카메라로 찍었을 때 저장될 파일명이므로 사용자 마음대로
+        // File 객체의 URI 를 얻는다.
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        {
+            String strpa = getApplicationContext().getPackageName();
+            cameraImageUri = FileProvider.getUriForFile(this, strpa + ".fileprovider", file);
+        }
+        else
+        {
+            cameraImageUri = Uri.fromFile(file);
+        }
+        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri);
+
+        if (!_isCapture)
+        { // 선택팝업 카메라, 갤러리 둘다 띄우고 싶을 때
+            Intent pickIntent = new Intent(Intent.ACTION_PICK);
+            pickIntent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+            pickIntent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            String pickTitle = "패턴 이미지를 선택하거나, 촬영하시겠습니까?";
+            Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+
+            // 카메라 intent 포함시키기..
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{intentCamera});
+            startActivityForResult(chooserIntent, FILECHOOSER_LOLLIPOP_REQ_CODE);
+        }
+        else
+        {// 바로 카메라 실행..
+            startActivityForResult(intentCamera, FILECHOOSER_LOLLIPOP_REQ_CODE);
+        }
     }
 }
